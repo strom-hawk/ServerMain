d105c1f406abb13be03e97f1b3c0a5493079f478
Saurav Suman1 <saurav.suman1@comviva.com>
2019-11-07 23:41:25 +0530

    logout request handled

---
+++ src/routes/LogoutUser.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+router.use(bodyParser.json());
+const name = "";
+
+router.post('/', (request, response) => {
+    console.log('/logOutUser');
+    const credentials = {
+        name : request.body.name,
+    }
+    console.log(credentials);
+    //CONNECTIVITY TO DB
+    //INSERT QUERY TO DB
+    //IF INSERTION IS SUCCESSFUL RETURN 200
+    //ELSE RETURN OTHER ERROR CODE
+
+    response.json({
+        "txnStatus":"200",
+        "txnMessage":"Sign Up Successful"
+    });
+})
+
+module.exports = router;
